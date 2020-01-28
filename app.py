99f1ebaffaa97dd19947f66196db4a246b17d552
Vincent Schuck <vschuck.pro@gmail.com>
2020-01-28 11:35:27 +0100

    les légendes de Bachir

--- app.py
+++ app.py
@@ -32,7 +32,35 @@ def get_object_by_id():
     try:
         obj_id = request.args.get('id')
         data = get_obj_by_id(obj_id)
-        resp  = json.dumps(data)
+        resp  = jsonify(data)
+        return resp
+
+    except Exception:
+        return traceback.format_exc()
+
+cors_gobc = CORS(app, resources={r"/get_object_by_const": {"origins": "*"}})
+
+@app.route('/get_object_by_const', methods=["GET"])
+@cross_origin(origin='*', headers=['Content-Type'])
+def get_object_by_const():
+    try:
+        const = request.args.get('const')
+        data = get_obj_by_const(const)
+        resp  = jsonify(data)
+        return resp
+
+    except Exception:
+        return traceback.format_exc()
+
+cors_gobd = CORS(app, resources={r"/get_object_by_dec": {"origins": "*"}})
+
+@app.route('/get_object_by_dec', methods=["GET"])
+@cross_origin(origin='*', headers=['Content-Type'])
+def get_object_by_dec():
+    try:
+        dec = request.args.get('dec')
+        data = get_obj_by_sadecdecdec(dec)
+        resp  = jsonify(data)
         return resp
 
     except Exception:
@@ -41,5 +69,5 @@ def get_object_by_id():
 #APP
 
 if __name__ == "__main__":
-   app.run(debug=True) ##Replaced with below code to run it using waitress
-   # serve(app, host='0.0.0.0', port=2727)
+   # app.run(debug=True) ##Replaced with below code to run it using waitress
+   serve(app, host='0.0.0.0', port=2727)
--- utils/requetes.py
+++ utils/requetes.py
@@ -2,12 +2,29 @@ from app import engine
 
 def get_obj_by_id(obj_id) :
     data = engine.execute(f'SELECT * FROM messier_catalog WHERE messier = "{obj_id}";')
-
     d, a = {}, []
     for rowproxy in data:
-        # rowproxy.items() returns an array like [(key0, value0), (key1, value1)]
         for column, value in rowproxy.items():
-            # build up the dictionary
+            d = {**d, **{column: value}}
+        a.append(d)
+
+    return a
+
+def get_obj_by_const(const) :
+    data = engine.execute(f'SELECT * FROM messier_catalog WHERE const = "{const}";')
+    d, a = {}, []
+    for rowproxy in data:
+        for column, value in rowproxy.items():
+            d = {**d, **{column: value}}
+        a.append(d)
+
+    return a
+
+def get_obj_by_sadecdecdec(dec) :
+    data = engine.execute(f'SELECT * FROM messier_catalog WHERE decouvreur = "{dec}";')
+    d, a = {}, []
+    for rowproxy in data:
+        for column, value in rowproxy.items():
             d = {**d, **{column: value}}
         a.append(d)
 
