2941b78aed558009645cdff5092328545ee587e6
Hugo Gomez <hugo@MacBook-Pro-de-Hugo.local>
2020-01-31 09:36:06 +0100

    general catalog added

--- scripts/jsontosql.py
+++ scripts/jsontosql.py
@@ -1,13 +1,28 @@
 import pandas as pd
 from sqlalchemy import create_engine 
 
+def push_json_to_db(catalog_name, catalog_file, columns_to_drop, engine):
+    df = pd.read_json(f"../raw_data/{catalog_file}")
+
+    json_fields = df.fields.to_json()
+    df = pd.read_json(json_fields).T
+    if len(columns_to_drop) > 0:
+        df = df.drop(columns=columns_to_drop)
+
+    df.to_sql(name=catalog_name, con=engine)
+
+# SQLite engine
 engine = create_engine('sqlite:///../db/galaxy-catalog.db', echo=False)
 
-messier_df = pd.read_json("../raw_data/messier-catalog.json")
+# Messier
+messier_catalog_file = "messier-catalog.json"
+messier_columns_to_drop = ["image_url", "mag"]
 
-json_fields = messier_df.fields.to_json()
-df = pd.read_json(json_fields).T
+# General
+gen_catalog_file = "general-catalog.json"
+gen_columns_to_drop = ["image"]
 
-df = df.drop(columns=["image_url", "mag"])
-
-df.to_sql(name="messier_catalog", con=engine)
+# Messier
+# push_json_to_db("messier_catalog", messier_catalog_file, messier_columns_to_drop, engine)
+# General
+push_json_to_db("general_catalog", gen_catalog_file, gen_columns_to_drop, engine)
