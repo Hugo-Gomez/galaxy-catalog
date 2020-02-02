0b5e730d06628aaba065d785dcf086c458e9e222
Hugo Gomez <hugo@macbook-pro-de-hugo.home>
2020-02-02 13:00:35 +0100

    response
    and http code

--- utils/requests.py
+++ utils/requests.py
@@ -1,14 +1,20 @@
 from app import engine
-from flask import jsonify
+from flask import jsonify, abort, Response
 
 # Generic method to collect object
 def get_object(catalog_name, attr, attr_value):
+    resp = []
     try:
         data = get_obj_by_attr(catalog_name, attr, attr_value)
         resp  = jsonify(data)
-        return resp
     except Exception:
-        return "<div>Invalid Request, find help at <a href='/'>this page</a>.</div>"
+        return Response(
+            "<div>Invalid Request, find help at <a href='/'>this page</a>.</div>", 
+            status=400)
+    if len(resp.json) == 0:
+        return abort(404)
+    else:
+        return resp
 
 # Get object by attribute
 def get_obj_by_attr(catalog_name, attr, attr_value):
